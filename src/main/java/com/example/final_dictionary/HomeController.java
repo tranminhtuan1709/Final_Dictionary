1fc1e93efeaed4b3494fa5764d43ebce86797d63
Phan Anh Tú <124888378+tuphan22028238@users.noreply.github.com>
2023-11-02 08:58:49 +0700

    merge 2 subproject

---
+++ src/main/java/com/example/final_dictionary/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.final_dictionary;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    @FXML
+    private AnchorPane homeAP;
+    @FXML
+    private TextField searchBar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
