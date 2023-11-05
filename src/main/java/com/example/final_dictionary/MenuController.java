ad03bd03dee6716d41266d1f43258635d0b3af26
Trần Minh Tuấn <minhtuan1231709@gmail.com>
2023-11-05 07:50:04 +0700

    Add setting option to Menu.fxml

--- src/main/java/com/example/final_dictionary/MenuController.java
+++ src/main/java/com/example/final_dictionary/MenuController.java
@@ -1,7 +1,6 @@
 package com.example.final_dictionary;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,11 +10,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
@@ -40,7 +37,6 @@ public class MenuController implements Initializable {
     private AnchorPane usernameAP;
     @FXML
     private Button usernameButton2;
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
